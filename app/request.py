51026b330d74fa1cb5503039d40c12c4eec5a55f
benjamin muchemi <benjamin.muchemi@student.moringaschool.com>
2021-08-08 19:13:27 +0300

    Listed the various names of news

--- app/request.py
+++ app/request.py
@@ -15,7 +15,7 @@ def get_data():
     '''
     Function that gets the json response to our url request
     '''
-    get_data_url = base_url.format(api_key)
+    get_data_url = 'https://newsapi.org/v2/sources?language=en&apiKey={}'.format(api_key)
 
     with urllib.request.urlopen(get_data_url) as url:
         get_data_data = url.read()
@@ -23,9 +23,31 @@ def get_data():
 
         data_results = None
 
-        if get_data_response['results']:
-            data_results_list = get_data_response['results']
+        if get_data_response['sources']:
+            data_results_list = get_data_response['sources']
             data_results = process_results(data_results_list)
 
+    return data_results
+
+
+def process_results(news_list):
+    '''
+    Function  that processes the movie result and transform them to a list of Objects
+
+    Args:
+        news_list: A list of dictionaries that contain news details
+
+    Returns :
+        data_results: A list of  objects
+    '''
+    data_results = []
+
+    for news_item in news_list:
+        id = news_item.get('id')
+        name = news_item.get('name')
+
+        if name:
+            news_object = Sources(id,name)
+            data_results.append(news_object)
 
     return data_results
--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from .request import get_data
 
 # Views
 @app.route('/')
@@ -8,5 +9,8 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
+
+    popular_news = get_data()
+    print(popular_news)
     title = 'Home - Welcome to The best News API'
-    return render_template('index.html', title = title)
+    return render_template('index.html', title = title,popular=popular_news)
