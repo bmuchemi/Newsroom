a8bd7e49234b024e0e08442682e39be822e23356
benjamin muchemi <benjamin.muchemi@student.moringaschool.com>
2021-08-08 17:12:11 +0300

    created a Sources class and initialised tests.

---
+++ app/models/sources.py
@@ -0,0 +1,9 @@
+class Sources:
+    '''
+    Sources class to define Sources Objects
+    '''
+
+    def __init__(self,id,name):
+        self.id =id
+        self.name = name
+       
---
+++ app/sources_test.py
@@ -0,0 +1,21 @@
+import unittest
+from models import sources
+Sources = sources.Sources
+
+class SourcesTest(unittest.TestCase):
+    '''
+    Test Class to test the behaviour of the Movie class
+    '''
+
+    def setUp(self):
+        '''
+        Set up method that will run before every Test
+        '''
+        self.new_source = Sources('bbc-news','BBC News')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_source,Sources))
+
+
+if __name__ == '__main__':
+    unittest.main()
