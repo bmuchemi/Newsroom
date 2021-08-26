8983bd8ce97d2707d690ab82b3d5214b14fc9bca
benjamin muchemi <benjamin.muchemi@student.moringaschool.com>
2021-08-26 14:53:24 +0300

    Initialise flask app

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,16 @@
 from flask import Flask
-from .config import DevConfig
-from flask_bootstrap import Bootstrap
+from config import config_options
 
-# Initializing application
-app = Flask(__name__,instance_relative_config = True)
+def create_app(config_name):
+    
+    app = Flask(__name__)
 
-# Setting up configuration
-app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
+    app.config.from_object(config_options[config_name])
 
-# Initializing Flask Extensions
-bootstrap = Bootstrap(app)
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
 
-from app import views
+    from .requests import configure_request
+    configure_request(app)
+
+    return app
