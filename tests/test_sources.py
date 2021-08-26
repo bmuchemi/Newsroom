c17d714dfb69ef6420b80f79c0c1ad36cc15df7a
benjamin muchemi <benjamin.muchemi@student.moringaschool.com>
2021-08-26 14:58:45 +0300

    Add test to check validity of news sources

---
+++ tests/test_sources.py
@@ -0,0 +1,22 @@
+import unittest
+from app.models import Sources
+
+class SourcesTest(unittest.TestCase):
+    '''
+    '''
+    def setUp(self):
+        '''
+        '''
+        self.new_source = Sources( 'bbc-news','BBC News')
+
+    def test_source(self):
+        '''
+        test for creation of a new instance
+        '''
+        self.assertTrue(isinstance(self.new_source, Sources))
+
+    def test_init(self):
+        '''
+        '''
+        self.new_source.id = 'bbc-news'
+        self.new_source.name='BBC News'
